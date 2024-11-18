46ca7d89c3d3ea32c766b03f0cc66262c1daa05f
Ananda Guedes do Ó <anandaguedesdoo@gmail.com>
2024-11-18 10:27:18 -0300

    arquivos de base

---
+++ DB4O-Agenda-dao/src/appconsole/Deletar.java
@@ -0,0 +1,32 @@
+package appconsole;
+
+import regras_negocio.Fachada;
+
+
+public class Deletar {
+
+	public Deletar(){
+		Fachada.inicializar();
+		try {
+			Fachada.excluirPessoa("jose");
+			System.out.println("apagou jose e seus telefones orfaos");
+			
+			Fachada.excluirTelefone("988881111");
+			System.out.println("apagou telefone...988881111");
+		} 
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		Fachada.finalizar();
+		System.out.println("fim do programa");
+	}
+
+
+
+	//=================================================
+	public static void main(String[] args) {
+		new Deletar();
+	}
+}
+
