eb9cb356b780813ce2366f134a65a95e2c7d8dd1
Ananda Guedes do Ó <anandaguedesdoo@gmail.com>
2024-12-02 19:51:17 -0300

    Update Cadastrar.java

--- DB4O-Agenda-dao/src/appconsole/Cadastrar.java
+++ DB4O-Agenda-dao/src/appconsole/Cadastrar.java
@@ -3,21 +3,23 @@ package appconsole;
 import java.util.ArrayList;
 import java.util.List;
 
+import modelo.Entregador;
+import modelo.Pedido;
 import regras_negocio.Fachada;
 
 public class Cadastrar {
 
 	public Cadastrar(){
 		try {
-			System.out.println("cadastrando pessoas...");
+			System.out.println("cadastrando...");
 			Fachada.inicializar();
 			
 			Fachada.criarPedido("17yv84", "01/01/2025",15.69, "Forma de bolo de cenoura");
 			Fachada.criarPedido("2753pk", "15/12/2024",100, "Suporte de monitor");
-			
-			Fachada.criarEntregador("1","João",new ArrayList<>(List.of()));
-			
-			Fachada.criarEntrega(1,"01/01/2025", "Avenida Vasco da Gama, 1245", new Entregador("Lays"), new Pedido("l1o574"));
+			//tirei o codigo que acompanhava entregador e não ia de acordo com fachada
+			Fachada.criarEntregador("João",new ArrayList<>(List.of()));
+			//coloquei o 1 entre aspas porque ele eh considerado String no metodo da fachada
+			Fachada.criarEntrega("1","01/01/2025", "Avenida Vasco da Gama, 1245", new Entregador("Lays"), new Pedido("l1o574"));
 			
 			System.out.println("Cadastrando");
 
@@ -35,5 +37,7 @@ public class Cadastrar {
 		new Cadastrar();
 	}
 }
+
+
 
 
